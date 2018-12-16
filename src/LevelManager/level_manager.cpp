6ca21b7547b8e294c887801f559b14b299b1d4d2
serradj <el-elhadi.serradj@etu.unistra.fr>
2018-12-16 14:01:24 +0100

    implement levelmanager en cour

--- src/LevelManager/level_manager.cpp
+++ src/LevelManager/level_manager.cpp
@@ -1,3 +1,34 @@
 #include "level_manager.h"
 
 
+void LevelManager::go_to_next_level(int lvl)
+{
+	if(!loadFileLevel(lvl))
+	{
+		//quelle truc qui verife qu'il ya plus de bmoc
+	}
+}
+
+void LevelManager::loadFileLevel( int lvl) // charger un fichier en foncionde son lvl
+{
+	Niveau.push("level1.txt");
+	Niveau.push("level2.txt");
+	Niveau.push("level3.txt");
+	Niveau.push("level4.txt");
+	
+	if(lvl <1 && lvl >4 )
+	{
+		std::cout<<"Level inconnu, lvl<1..4>" <<std::endl;
+	}
+	switch(lvl)
+	{
+		case 1 : return Niveau.get(1);
+		break;
+		case 2 : return Niveau.get(2);
+		break;
+		case 3 : return Niveau.get(3);
+		break;
+		case 4 : return Niveau.get(4);
+		break;
+	}
+}
